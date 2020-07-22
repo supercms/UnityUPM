2f0bb3a0c322478c9f904fc4d2592e7e265e7742
supercms <supercms@naver.com>
2020-07-22 14:33:32 +0900

    +singleton

---
+++ Packages/supercmsSdk/Runtime/CmsSingleton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CmsSingleton<T> : MonoBehaviour where T: MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var obj = FindObjectOfType<T>();
+                if (obj != null)
+                {
+                    instance = obj;
+                }
+                else
+                {
+                    var newObj = new GameObject().AddComponent<T>();
+                    instance = newObj;
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        var objs = FindObjectsOfType<T>();
+        if (objs.Length != 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
